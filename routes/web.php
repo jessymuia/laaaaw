0727290ac8b316f841eb162fa78c5b671ad7106e
Jessy Muia <kiambachris29@gmail.com>
2026-08-24 13:39:56 +0300

    .

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\AppController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/{any}', [AppController::class, 'index'])
-    ->where('any', '.*')
-    ->name('login');
